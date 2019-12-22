1d8ac1a49fc195ce09031bfde5b03530da493aef
sixDawn <1355016403@qq.com>
2019-12-22 15:43:09 +0800

    16

--- src/services/axios.js
+++ src/services/axios.js
@@ -56,6 +56,9 @@ service.interceptors.response.use(
       }
     },
     error => {
+      if (!error.response) {
+        return Promise.reject(error);
+      }
       if (error.response.status) {
         switch (error.response.status) {
           case 500:
